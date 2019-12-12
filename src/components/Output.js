3d131220814e64bc109b55469aadb32eb3f3a793
Fredrik H <fredrik@pool.fi>
2019-12-12 08:56:58 +0200

    first commit

---
+++ src/components/Output.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Output extends Component {
+  render() {
+    let height = this.props.data.height;
+    let weight = this.props.data.weight;
+    let bmi = this.props.data.bmi;
+    let bmiClass = this.props.data.bmiClass;
+    return (
+      <div className="output">
+        <h3>{height} cm</h3>
+        <h3>{weight} kg</h3>
+        <h3>BMI {bmi}</h3>
+        <h3 className={(this.props.data.bmiClass === "Obese") ? "danger" : ""}>{bmiClass} {(this.props.data.bmiClass === "Obese") ? <a href='http://www.someproductsit.com'>What can I do?</a> : ""}</h3>
+      </div>
+    );
+  }
+}
+
+export default Output;
